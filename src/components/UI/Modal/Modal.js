598171487c2719278392e1d571a11987849c7ea9
MatAff <mathijsaffourtit@gmail.com>
2018-05-17 20:46:20 -0600

    Latest changes

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,18 +1,24 @@
 import React from 'react';
 
 import classes from './Modal.css';
+import Aux from '../../../hoc/Aux';
+import Backdrop from '../Backdrop/Backdrop';
 
 const Modal = (props) => {
     return (
-        <div
-          className={classes.Modal}
-          style={{
-              transform : props.show ? 'translateY(0)' : 'translateY(-100vh)',
-              opacity : props.show ? '1' : '0'
-          }}>
+      <Aux>
+        <Backdrop show={props.show}
+          <div
+            className={classes.Modal}
+            style={{
+                transform : props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                opacity : props.show ? '1' : '0'
+            }}>
 
-          {props.children}
-        </div>
+            {props.children}
+          </div>
+        </Backdrop>
+      </Aux>
     );
 };
 
